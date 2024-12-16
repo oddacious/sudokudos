3775b43e6ecd71e4c751831d2d7937f1d2b6c85c
rdj <fumble.to.victory@gmail.com>
2024-12-16 17:03:50 -0500

    Adjust heroku startup script

--- heroku_setup.sh
+++ heroku_setup.sh
@@ -1,8 +1,9 @@
 mkdir -p ~/.streamlit/
-cp ./.streamlit/config.toml ~/.streamlit/config.toml
+cp ./.heroku/streamlit_config.toml ~/.streamlit/config.toml
 
-echo "\n\
-[server]\n\
-headless=true\n\
-port=$PORT\n\
-" >> ~/.streamlit/config.toml
+echo $PORT
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    sed -i '' 's/port=.*/port='$PORT'/' ~/.streamlit/config.toml
+else
+    sed -i 's/port=.*/port='$PORT'/' ~/.streamlit/config.toml
+fi
